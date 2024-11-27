ed54b6301353f8b919b331addca68cca598137e2
wangguangwu <wangguangwu@pukanghealth.com>
2024-11-27 09:57:15 +0800

    pom文件提交

---
+++ spring-security-with-custom-login-page-demo/src/main/java/com/wangguangwu/springsecuritywithcustomloginpagedemo/SpringSecurityWithCustomLoginPageDemoApplication.java
@@ -0,0 +1,16 @@
+package com.wangguangwu.springsecuritywithcustomloginpagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangguangwu
+ */
+@SpringBootApplication
+public class SpringSecurityWithCustomLoginPageDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityWithCustomLoginPageDemoApplication.class, args);
+    }
+
+}
