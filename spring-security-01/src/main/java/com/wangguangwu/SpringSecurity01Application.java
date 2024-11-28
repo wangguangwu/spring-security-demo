15d8af392129eab014dcce30caafdc3e53551460
wangguangwu <wangguangwu@pukanghealth.com>
2024-11-28 21:59:40 +0800

    给 Spring Security 01 新增 README.md

--- spring-security-01/src/main/java/com/wangguangwu/SpringSecurity01Application.java
+++ spring-security-01/src/main/java/com/wangguangwu/SpringSecurity01Application.java
@@ -1,4 +1,4 @@
-package com.wangguangwu.springsecurity01;
+package com.wangguangwu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-security-01/src/main/java/com/wangguangwu/controller/HelloController.java
+++ spring-security-01/src/main/java/com/wangguangwu/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.wangguangwu.springsecurity01.controller;
+package com.wangguangwu.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
